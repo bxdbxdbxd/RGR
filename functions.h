a721a42d2fda0fe1cdc34853264c9a833866fdfb
Time:D <darya.sam54@gmail.com>
2024-06-07 02:11:50 +0700

    betaTest fun

---
+++ functions.h
@@ -0,0 +1,38 @@
+#include <string>
+#include <iostream>
+
+void first(std::string data){
+    std::cout << "first" << std::endl << data << std::endl << "first";
+}
+
+void second(std::string data){
+    std::cout << "second" << std::endl << data << std::endl << "second";
+}
+
+void third(std::string data){
+    std::cout << "third" << std::endl << data << std::endl << "third";
+}
+
+void fourth(std::string data){
+    std::cout << "fourth" << std::endl << data << std::endl << "fourth";
+}
+
+void fifth(std::string data){
+    std::cout << "fifth" << std::endl << data << std::endl << "fifth";
+}
+
+void sixth(std::string data){
+    std::cout << "sixth" << std::endl << data << std::endl << "sixth";
+}
+
+void seventh(std::string data){
+    std::cout << "seventh" << std::endl << data << std::endl << "seventh";
+}
+
+void eighth(std::string data){
+    std::cout << "eighth" << std::endl << data << std::endl << "eighth";
+}
+
+void ninth(std::string data){
+    std::cout << "ninth" << std::endl << data << std::endl << "ninth";
+}
