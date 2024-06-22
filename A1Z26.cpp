e4d7e727fd7f651bfa443fbb48ba34d00ee6530d
Fedor <fedortk01@yandex.ru>
2024-06-22 22:07:09 +0600

    inter atage

--- A1Z26.cpp
+++ A1Z26.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
+#include <locale.h>
+#include <windows.h>
 
 using namespace std;
 
 int main() {
+  SetConsoleCP(1251);
+  SetConsoleOutputCP(1251);
+  setlocale(LC_ALL, "Russian");
   string plaintext;
   cout << "Введите текст для шифрования: ";
   getline(cin, plaintext);
@@ -10,7 +16,7 @@ int main() {
   string ciphertext;
   for (char c : plaintext) {
     if (isalpha(c)) {
-      int num = (tolower(c) - 'a' + 1);
+      int num = (tolower(c) - 'a' + 1)%255;
       ciphertext += to_string(num) + " ";
     } else {
       ciphertext += c;
