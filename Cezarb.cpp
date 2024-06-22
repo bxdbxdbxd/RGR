0c30d202cdbf313c82bf52e59fdafdeadb7e1f0f
Fedor <fedortk01@yandex.ru>
2024-06-22 20:44:23 +0600

    cezarb

--- Cezarb.cpp
+++ Cezarb.cpp
@@ -4,31 +4,49 @@
 #include <windows.h>
 using namespace std;
 
-wstring cypher(wstring text, int step);
-wstring decypher(wstring text, int step);
+string cypher(string text, int step,int max);
+string decypher(string text, int step,int max);
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL,"russian");
     int step=0;
-    wstring text;
-    wcin>>text;
+    int kol=255;
+    int var=0;
+    string text;
+    getline(cin, text);
     cout << "Enter the key value: ";
     cin >> step;
-    text=cypher(text,step);
-    wcout << text <<endl;
+    cout << "choose cypher(1) or decypher(2): ";
+    cin>>var;
+    switch(var){
+        case 1:
+        text=cypher(text,step,kol);
+        cout << text <<endl;
+        break;
+        case 2:
+        text=decypher(text,step,kol);
+        cout << text <<endl;
+        break;
+        default:
+        cout<<"You must make correct choice ";
+        break;
+    }
+
     return 0;
 }
-wstring cypher(wstring text, int step){
+string cypher(string text, int step,int max){
     for(int i=0;i<text.size();i++){
         text[i]=text[i]+step;
+        text[i]=text[i]%max;
     }
     return text;
 }
-wstring decypher(wstring text, int step){
+string decypher(string text, int step,int max){
     for(int i=0;i<text.size();i++){
         text[i]=text[i]-step;
+        text[i]=text[i]%max;
     }
     return text;
 }
